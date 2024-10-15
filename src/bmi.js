9e1c599c09ceb5c249f70de30023c1ea9c6d95de
Mary <marynevermann@gmail.com>
2024-10-15 04:07:18 +0200

    ensured the gender is selected

--- src/bmi.js
+++ src/bmi.js
@@ -1,17 +1,32 @@
 function calculateBmi(event){
     //Getting the values from input fields
-    let height = parseFloat(document.getElementById("bodyHeight").value /100);// to convert the value in meters I did /100
-    let weight = parseFloat(document.querySelector("bodyWeight").value);
-    let lastname = document.getElementById("lastName").value;
-    let firstname = document.getElementById("firstName").value;
-    let date = document.getElementById("birthDate").value;
-    let gender = document.querySelector('input[name="gender]:checked');
+    let height = parseFloat(document.getElementById('bodyHeight').value /100);// to convert the value in meters I did /100
+    let weight = parseFloat(document.getElementById('bodyWeight').value);
+    let lastname = document.getElementById('lastName').value;
+    let firstname = document.getElementById('firstName').value;
+    let date = document.getElementById('birthDate').value;
+
+    //displaying the selected radio /// find the problem hier later
+    let gender = document.getElementsByName('gender');
+    let selectedGender = null;
+    for(let radio of gender){
+        if(radio.checked){
+            selectedGender = radio.value;
+            break;
+        }
+    }
 
     //checking if the valuees exist and are valid numbers
     if(height && weight){
-        let bmi = weight / (height*height);
+        //ensure that the gender is selected
+        if(!selectedGender){
+            document.getElementById('result').innerHTML ="please select a gender.";
+            return;
+        }
+        //bmi calculation
+        let bmi = weight / (height * height);
         let bmiLevel = Math.round(bmi);
-        let output = `${lastname} ${firstname}, born on the ${date} with the gender${gender.value}, your BMI level is: ${bmiLevel}.`;
+        let output = `${lastname} ${firstname}, born on the ${date} with the gender ${selectedGender}, your BMI level is: ${bmiLevel}.`;
         //check the BMI level and update the message accordingly
         if(bmiLevel >= 35){
             output+="You suffer from Adipositas Grad 2. We recommend you see a doctor!";
@@ -24,13 +39,13 @@ function calculateBmi(event){
         }
 
         //Display the result
-        document.getElementById("result").innerHTML = output;
+        document.getElementById('result').innerHTML = output;
     }else{
         //handle invalid input
-        document.getElementById("result").innerHTML = "please enter valid height and weight.";
+        document.getElementById('result').innerHTML = "please enter valid height and weight.";
     }
 }
     
-
+//set up event listener for the button
 let buttonElement = document.querySelector("#button");
     buttonElement.addEventListener("click", calculateBmi);
